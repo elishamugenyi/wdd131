dcd84df199515415988089f964402ca5d0cbe31c
elishamugenyi <elishamugenyi5@gmail.com>
2024-04-24 22:53:53 +0300

    deleted

--- week01/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var footer = document.querySelector('footer');
-    var lastModified = document.lastModified;
-    footer.querySelector('#lastModified').textContent = 'Last Modification Date: ' + lastModified;
-});
