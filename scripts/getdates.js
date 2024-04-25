ce977fdf28e745b1ffd7cc022c05242ad2d66aed
elishamugenyi <elishamugenyi5@gmail.com>
2024-04-25 22:42:30 +0300

    created code to auto populate date and year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var footer = document.querySelector('footer');
-    var lastModified = document.lastModified;
-    footer.querySelector('#lastModified').textContent = 'Last Modification Date: ' + lastModified;
+    
+    // Get the current date
+    var currentDate = new Date();
+    var year = currentDate.getFullYear(); // Get the current year
+    
+    // Construct the copyright message
+    var copyrightMessage = '\u00A9' + year + ' - Elisha Mugenyi \uD83C\uDF3A Uganda'; // Using Unicode for copyright and flower emoji
+    
+    // Set the copyright message in the footer
+    footer.querySelector('#lastModified').textContent = copyrightMessage;
 });
