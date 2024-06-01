e1bec7450ec69a5c46e48a554b15cd90deb5d304
elishamugenyi <elishamugenyi5@gmail.com>
2024-06-01 23:53:42 +0300

    added faq javascript

---
+++ project/scripts/faq.js
@@ -0,0 +1,15 @@
+//js code to handle faq links clicked by user
+document.addEventListener('DOMContentLoaded', function(){
+    const faqItems = document.querySelectorAll('.faq-item h3');
+    faqItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const answer = item.nextElementSibling;
+            if (answer.style.display === 'block') {
+                answer.style.display = 'none';
+            } else {
+                answer.style.display = 'block';
+            }
+        });
+    });
+
+});
